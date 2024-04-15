72c109fb9d803b0ede25be5e2f24ea96397f0118
PikeCaptain <idunno6418@gmail.com>
2024-04-15 14:33:06 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Game
+    {
+
+        public string title, genre;
+        
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game myGame = new Game("Forza Horizon 5", "E for everyone");
+
+            Console.WriteLine($"My favorite game is {myGame.title} and it is rated {myGame.genre}!");
+
+            Game myOtherGame = new Game();
+
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is rated {myOtherGame.genre} game!");
+        }
+    }
+}
